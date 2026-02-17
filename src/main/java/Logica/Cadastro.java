001d3b776f99cc6fcd605b93585854c57647978c
EdsonFSalles <Edson.7slles@gmail.com>
2026-02-17 13:44:02 -0300

    Adicionando Cases 1 | 2

--- src/main/java/Logica/Cadastro.java
+++ src/main/java/Logica/Cadastro.java
@@ -7,52 +7,90 @@ import Conexao.DAO;
 public class Cadastro {
 	DAO<Usuario> dao = new DAO<>();
 	Scanner entrada;
-	
+
 	public Cadastro() {
 		entrada = new Scanner(System.in);
 		String borda = "==".repeat(15);
-		System.out.println("Qual ação deseja realizar?");
+		System.out.println(borda + "\n Qual ação deseja realizar?");
 		System.out.println(" 1 - Cadastrar \n 2 - Atualizar cadastro \n 3 - Buscar Cadastro"
-				+ "\n 4 - Deletear cadastro \n 5 - Sair");
+				+ "\n 4 - Deletear cadastro \n 5 - Sair" + "\n" + borda);
 		Integer opcao = entrada.nextInt();
-		switch(opcao) {
-		case 1: 
-			System.out.println(" Nome: ");
-			String nome = entrada.next();
-			System.out.println("\n Email: ");
-			String email = entrada.next();
-			System.out.println("\n Idade: ");
-			int idade = entrada.nextInt();
-			System.out.println("\n Senha: ");
-			String senha = entrada.next();
-			
+
+		switch (opcao) {
+		case 1:
+			String nome = lerString("Nome");
+			String email = lerString("Email");
+			int idade = lerInt("Idade");
+			String senha = lerString("Senha");
+
 			Usuario usuario = new Usuario(nome, idade, email, senha);
 			cadastrarUsuario(usuario);
-			
+
 		case 2:
+			System.out.println("Digite o Id do usuario: ");
+			long IdUsuario = entrada.nextLong();
+			Usuario usuarioNoBanco = bucarCadastro(IdUsuario);
+
+			if (usuarioNoBanco == null) {
+				System.out.println("Usuario não encontrado!");
+				return;
+			} else {
+				System.out.println("== USUARIO CADASTRADO ==");
+				System.out.println("Nome: " + usuarioNoBanco.getNome());
+				System.out.println("Email: " + usuarioNoBanco.getEmail());
+				System.out.println("Idade: " + usuarioNoBanco.getIdade());
+			}
+
+			System.out.println("Digite Atualização: (Pule o registro com enter)");
+			String Nome = lerString("Nome");
+			String Email = lerString("Email");
+			int Idade = lerInt("Idade");
+
+			String pergunta = lerString("\n Deseja atualizar senha? sim | não").toLowerCase();
+			String novaSenha = null;
+			
+			if (pergunta.equals("sim")) {
+				String Senha = lerString("Nova Senha");
+				usuarioNoBanco.setSenha(Senha);
+				novaSenha = Senha;
+			}
+
+			Usuario usuarioAtualizado = new Usuario(Nome, Idade, Email, novaSenha);
+			atualizarCadastro(usuarioAtualizado);
+
 		case 3:
 		case 4:
 		case 5:
 		}
 	}
-	
-	
+
+	private String lerString(String texto) {
+		System.out.println("\n " + texto + ":");
+		return entrada.next();
+	}
+
+	private int lerInt(String texto) {
+		System.out.println("\n " + texto + ":");
+		return entrada.nextInt();
+	}
+
 	private void cadastrarUsuario(Usuario usuario) {
 		dao.abrirT().incluir(usuario).fecharT();
 	}
-	
+
 	private void atualizarCadastro(Usuario usuario) {
-		 dao.abrirT().Atualizar(usuario).fecharT();
+		dao.abrirT().Atualizar(usuario).fecharT();
 	}
-	
-	private void deletarCadastro(Usuario usuario){
-		if(usuario.getNome().isEmpty() && (usuario.getId() != null)) {
+
+	private void deletarCadastro(Usuario usuario) {
+		if (usuario.getNome().isEmpty() && (usuario.getId() != null)) {
 			dao.abrirT().deletarCadastro(usuario.getId()).fecharT();
 		}
 	}
-	
-	private Usuario bucarCadastro(Usuario usuario){
-		return (Usuario) dao.abrirT().obterPorID(usuario.getId());
+
+	private Usuario bucarCadastro(Long Id) {
+		return dao.abrirT().obterPorID(Id);
+
 	}
-	
+
 }
--- src/main/java/Logica/Usuario.java
+++ src/main/java/Logica/Usuario.java
@@ -9,61 +9,67 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "UsuarioCadastrado")
 public class Usuario {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private int idade;
-	private String Email;
+	private String email;
 	private String senha;
-	
+
 	public Usuario() {
 	}
 
-	
-	
-	public Usuario(String nome, int idade, String email, String senha) {
-		super();
+	public Usuario(String nome, int idade, String email) {
 		this.nome = nome;
 		this.idade = idade;
-		Email = email;
-		this.senha = senha;
+		this.email = email;
 	}
 
-
+	public Usuario(String nome, int idade, String email, String senha) {
+		this(nome, idade, email);
+		this.senha = senha;
+	}
 
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public int getIdade() {
 		return idade;
 	}
+
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
+
 	public String getEmail() {
-		return Email;
+		return email;
 	}
+
 	public void setEmail(String email) {
-		Email = email;
+		this.email = email;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
-	
-	
+
 }
